b7e672eb59ee6e9f5f09114fa21df12111e94262
Srijitadebnath <SRIJITA.13103@stu.upes.ac.in>
2026-06-19 06:22:04 +0530

    Day_5 submission2

--- DSA/Srijita_590013103/Day_5/Question1_leetcode.cpp
+++ DSA/Srijita_590013103/Day_5/Question1_leetcode.cpp
@@ -23,15 +23,14 @@ void operator delete[](void *, size_t) noexcept {}
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int farthest = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            if(i > farthest)
-                return false;
-            farthest = max(farthest, i + nums[i]);
+     
+    int reach = 0;
 
-            if(farthest >= nums.size() - 1)
-                return true;
-        }
-        return true;  
+    for(int i = 0; i < nums.size(); i++) {
+        if(i > reach) return false;
+        reach = max(reach, i + nums[i]);
     }
+
+    return true;
+}
 };
